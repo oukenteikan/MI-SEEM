13fde6120ecb0572f3584ecaeffebff586bccfea
wangqian1 <wangqian1@sensetime.com>
2019-05-12 11:34:50 +0800

    index

--- econcs/views.py
+++ econcs/views.py
@@ -10,3 +10,27 @@ def to_index(request):
 def index(request):
     response = render_to_response('econcs_index.html')
     return response 
+
+def home(request):
+    response = render_to_response('home.html')
+    return response
+
+def people(request):
+    response = render_to_response('people.html')
+    return response
+
+def publication(request):
+    response = render_to_response('publication.html')
+    return response
+
+def teaching(request):
+    response = render_to_response('teaching.html')
+    return response
+
+def seminar(request):
+    response = render_to_response('seminar.html')
+    return response
+
+def news(request):
+    response = render_to_response('news.html')
+    return response
