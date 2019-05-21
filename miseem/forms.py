9a035ae5787c96b1f47e4229ce4673a58a1c863a
wangqian1 <wangqian1@sensetime.com>
2019-05-21 12:33:12 +0800

    awesome

--- miseem/forms.py
+++ miseem/forms.py
@@ -1,75 +1,22 @@
-from django.db.models import Q
 from django.forms import *
-from .models import *
-from random import randint, sample
 
 class PointWise(Form):
-    total_num = Sentence.objects.count()-1
-    chosen_id = randint(0, total_num)
-    chosen_sentence = Sentence.objects.all()[chosen_id]
-    chosen_question = chosen_sentence.belong_to_question
-    chosen_sentence_pk = IntegerField(widget = HiddenInput(), initial=chosen_sentence.pk, disabled = True)
-    description = CharField(widget = TextInput(attrs={'size': '128'}), initial=chosen_question.description, disabled = True)
-    standard = CharField(widget = TextInput(attrs={'size': '128'}), initial=chosen_question.standard, disabled = True)
-    sentence = CharField(widget = TextInput(attrs={'size': '128'}), initial=chosen_sentence.content, disabled = True)
-    score = FloatField(max_value = 10, min_value = 0)
+    absolute_score = FloatField(max_value = 10, min_value = 0)
 
 class PairWise(Form):
-    chosen_sentence_list = []
-    total_question_num = Question.objects.count()-1
-    total_system_num = System.objects.count() - 1
-    while len(chosen_sentence_list) < 2:
-        chosen_system_list = sample(list(System.objects.all()), 2)
-        chosen_system_1 = chosen_system_list[0]
-        chosen_system_2 = chosen_system_list[1]
-        chosen_question = sample(list(Question.objects.all()), 1)[0]
-        chosen_sentence_list = Sentence.objects.filter(belong_to_question = chosen_question).filter(Q(belong_to_system = chosen_system_1) | Q(belong_to_system = chosen_system_2))
-    chosen_sentence = sample(list(chosen_sentence_list), 2) 
-    chosen_sentence_1 = chosen_sentence[0]
-    chosen_sentence_2 = chosen_sentence[1]
-
-    chosen_sentence_pk_1 = IntegerField(widget = HiddenInput(), initial=chosen_sentence_1.pk, disabled = True)
-    chosen_sentence_pk_2 = IntegerField(widget = HiddenInput(), initial=chosen_sentence_2.pk, disabled = True)
-    description = CharField(widget = TextInput(attrs={'size': '128'}), initial=chosen_question.description, disabled = True)
-    standard = CharField(widget = TextInput(attrs={'size': '128'}), initial=chosen_question.standard, disabled = True)
-    first = CharField(widget = TextInput(attrs={'size': '128'}), initial=chosen_sentence_1.content, disabled = True)
-    second = CharField(widget = TextInput(attrs={'size': '128'}), initial=chosen_sentence_2.content, disabled = True)
-    score = ChoiceField(
+    relative_score = ChoiceField(
         choices=((1, 'The first is better.'), (-1, 'The second is better.'), (0, 'I think it is a tie.')),
         widget=RadioSelect
     )
 
-class ListWise(forms.Form):
-    chosen_sentence_list = []
-    total_question_num = Question.objects.count()-1
-    total_system_num = System.objects.count() - 1
-    while len(chosen_sentence_list) < 3:
-        chosen_system_list = sample(list(System.objects.all()), 3) 
-        chosen_system_1 = chosen_system_list[0]
-        chosen_system_2 = chosen_system_list[1]
-        chosen_system_3 = chosen_system_list[2]
-        chosen_question = sample(list(Question.objects.all()), 1)[0]
-        chosen_sentence_list = Sentence.objects.filter(belong_to_question = chosen_question).filter(Q(belong_to_system = chosen_system_1) | Q(belong_to_system = chosen_system_2) | Q(belong_to_system = chosen_system_3))
-    chosen_sentence = sample(list(chosen_sentence_list), 3) 
-    chosen_sentence_1 = chosen_sentence[0]
-    chosen_sentence_2 = chosen_sentence[1]
-    chosen_sentence_3 = chosen_sentence[2]
-
-    chosen_sentence_pk_1 = IntegerField(widget = HiddenInput(), initial=chosen_sentence_1.pk, disabled = True)
-    chosen_sentence_pk_2 = IntegerField(widget = HiddenInput(), initial=chosen_sentence_2.pk, disabled = True)
-    chosen_sentence_pk_3 = IntegerField(widget = HiddenInput(), initial=chosen_sentence_3.pk, disabled = True)
-    description = CharField(widget = TextInput(attrs={'size': '128'}), initial=chosen_question.description, disabled = True)
-    standard = CharField(widget = TextInput(attrs={'size': '128'}), initial=chosen_question.standard, disabled = True)
-    first = CharField(widget = TextInput(attrs={'size': '128'}), initial=chosen_sentence_1.content, disabled = True)
-    second = CharField(widget = TextInput(attrs={'size': '128'}), initial=chosen_sentence_2.content, disabled = True)
-    third = CharField(widget = TextInput(attrs={'size': '128'}), initial=chosen_sentence_3.content, disabled = True)
-    score = ChoiceField(
-        choices=((1, 'The first is the best and the second is the worst.'), 
-                (2, 'The first is the best and the third is the worst.'),
-                (3, 'The second is the best and the first is the worst.'),
-                (4, 'The second is the best and the third is the worst.'),
-                (5, 'The third is the best and the first is the worst.'),
-                (6, 'The third is the best and the second is the worst.')),
+class ListWise(Form):
+    rank_score = ChoiceField(
+        choices=((1, '132(The first is the best and the second is the worst.)'), 
+                (2, '123(The first is the best and the third is the worst.)'),
+                (3, '231(The second is the best and the first is the worst.)'),
+                (4, '213(The second is the best and the third is the worst.)'),
+                (5, '321(The third is the best and the first is the worst.)'),
+                (6, '312(The third is the best and the second is the worst.)')),
         widget=RadioSelect
     )
 
--- miseem/views.py
+++ miseem/views.py
@@ -4,6 +4,8 @@ from .models import *
 from .forms import *
 from django.template import *
 from datetime import datetime
+from django.db.models import Q
+from random import randint, sample
 
 # Create your views here.
 
@@ -17,36 +19,71 @@ def index(request):
 def example(request, current):
     if request.COOKIES.get("visited_example"+str(current)):
         return render(request, 'sorry.html')
+    context = {}
+    context['current'] = current
+    if current == 1:
+        context['head'] = "The 1st example!"
+    elif current == 2:
+        context['head'] = "The 2nd example!"
+    elif current == 3:
+        context['head'] = "The 3rd example!"
+    else:
+        return render(request, 'sorry.html')
     if request.method == "GET":
-        context = {}
-        context['current'] = current
         if current == 1:
-            context['head'] = "The 1st example!"
+            total_num = Sentence.objects.count()-1
+            chosen_id = randint(0, total_num)
+            chosen_sentence = Sentence.objects.all()[chosen_id]
+            chosen_question = chosen_sentence.belong_to_question
+            context['chosen_sentence'] = [(chosen_sentence.pk, chosen_sentence.content)]
+            context['chosen_description'] = chosen_question.description
+            context['chosen_standard'] = chosen_question.standard
             context['form'] = PointWise()
         elif current == 2:
-            context['head'] = "The 2nd example!"
+            chosen_sentence_list = []
+            total_question_num = Question.objects.count()-1
+            total_system_num = System.objects.count() - 1
+            while len(chosen_sentence_list) < 2:
+                chosen_system_list = sample(list(System.objects.all()), 2)
+                chosen_system_1 = chosen_system_list[0]
+                chosen_system_2 = chosen_system_list[1]
+                chosen_question = sample(list(Question.objects.all()), 1)[0]
+                chosen_sentence_list = Sentence.objects.filter(belong_to_question = chosen_question).filter(Q(belong_to_system = chosen_system_1) | Q(belong_to_system = chosen_system_2))
+            chosen_sentence = sample(list(chosen_sentence_list), 2) 
+            chosen_sentence_1 = chosen_sentence[0]
+            chosen_sentence_2 = chosen_sentence[1]
+            context['chosen_sentence'] = [(chosen_sentence_1.pk, chosen_sentence_1.content), 
+                                        (chosen_sentence_2.pk, chosen_sentence_2.content)]
+            context['chosen_description'] = chosen_question.description
+            context['chosen_standard'] = chosen_question.standard            
             context['form'] = PairWise()
         elif current == 3:
-            context['head'] = "The 3rd example!"
+            chosen_sentence_list = []
+            total_question_num = Question.objects.count()-1
+            total_system_num = System.objects.count() - 1
+            while len(chosen_sentence_list) < 3:
+                chosen_system_list = sample(list(System.objects.all()), 3) 
+                chosen_system_1 = chosen_system_list[0]
+                chosen_system_2 = chosen_system_list[1]
+                chosen_system_3 = chosen_system_list[2]
+                chosen_question = sample(list(Question.objects.all()), 1)[0]
+                chosen_sentence_list = Sentence.objects.filter(belong_to_question = chosen_question).filter(Q(belong_to_system = chosen_system_1) | Q(belong_to_system = chosen_system_2) | Q(belong_to_system = chosen_system_3))
+            chosen_sentence = sample(list(chosen_sentence_list), 3) 
+            chosen_sentence_1 = chosen_sentence[0]
+            chosen_sentence_2 = chosen_sentence[1]
+            chosen_sentence_3 = chosen_sentence[2]
+            context['chosen_sentence'] = [(chosen_sentence_1.pk, chosen_sentence_1.content), 
+                                        (chosen_sentence_2.pk, chosen_sentence_2.content), 
+                                        (chosen_sentence_3.pk, chosen_sentence_3.content)]
+            context['chosen_description'] = chosen_question.description
+            context['chosen_standard'] = chosen_question.standard
             context['form'] = ListWise()
         else:
             return render(request, 'sorry.html')
         response = render(request, 'example.html', context)
-    
     elif request.method == "POST":
-        context = {}
-        context['current'] = current
-        if current == 1:
-            context['form'] = PointWise(request.POST)
-        elif current == 2:
-            context['form'] = PairWise(request.POST)
-        elif current == 3:
-            context['form'] = ListWise(request.POST)
-        else:
-            return render(request, 'sorry.html')
         response = render(request, 'example.html', context)
         response.set_cookie("visited_example"+str(current), True)
-
     else:
         response = render(request, 'sorry.html')
     return response
@@ -72,11 +109,82 @@ def entry(request):
     return response
 
 def task(request):
-    if request.COOKIES.get("visited_task"):
+    current = randint(1, 3)
+    context = {}
+    context['current'] = current
+    if current == 1:
+        context['head'] = "The 1st type of task!"
+    elif current == 2:
+        context['head'] = "The 2nd type of task!"
+    elif current == 3:
+        context['head'] = "The 3rd type of task!"
+    else:
         return render(request, 'sorry.html')
-    response = render(request, 'task.html')
-    if request.method == "POST":
+    if request.method == "GET":
+        if current == 1:
+            total_num = Sentence.objects.count()-1
+            chosen_id = randint(0, total_num)
+            chosen_sentence = Sentence.objects.all()[chosen_id]
+            chosen_question = chosen_sentence.belong_to_question
+            context['chosen_sentence'] = [(chosen_sentence.pk, chosen_sentence.content)]
+            context['chosen_description'] = chosen_question.description
+            context['chosen_standard'] = chosen_question.standard
+            context['form'] = PointWise()
+        elif current == 2:
+            chosen_sentence_list = []
+            total_question_num = Question.objects.count()-1
+            total_system_num = System.objects.count() - 1
+            while len(chosen_sentence_list) < 2:
+                chosen_system_list = sample(list(System.objects.all()), 2)
+                chosen_system_1 = chosen_system_list[0]
+                chosen_system_2 = chosen_system_list[1]
+                chosen_question = sample(list(Question.objects.all()), 1)[0]
+                chosen_sentence_list = Sentence.objects.filter(belong_to_question = chosen_question).filter(Q(belong_to_system = chosen_system_1) | Q(belong_to_system = chosen_system_2))
+            chosen_sentence = sample(list(chosen_sentence_list), 2) 
+            chosen_sentence_1 = chosen_sentence[0]
+            chosen_sentence_2 = chosen_sentence[1]
+            context['chosen_sentence'] = [(chosen_sentence_1.pk, chosen_sentence_1.content), 
+                                        (chosen_sentence_2.pk, chosen_sentence_2.content)]
+            context['chosen_description'] = chosen_question.description
+            context['chosen_standard'] = chosen_question.standard            
+            context['form'] = PairWise()
+        elif current == 3:
+            chosen_sentence_list = []
+            total_question_num = Question.objects.count()-1
+            total_system_num = System.objects.count() - 1
+            while len(chosen_sentence_list) < 3:
+                chosen_system_list = sample(list(System.objects.all()), 3) 
+                chosen_system_1 = chosen_system_list[0]
+                chosen_system_2 = chosen_system_list[1]
+                chosen_system_3 = chosen_system_list[2]
+                chosen_question = sample(list(Question.objects.all()), 1)[0]
+                chosen_sentence_list = Sentence.objects.filter(belong_to_question = chosen_question).filter(Q(belong_to_system = chosen_system_1) | Q(belong_to_system = chosen_system_2) | Q(belong_to_system = chosen_system_3))
+            chosen_sentence = sample(list(chosen_sentence_list), 3) 
+            chosen_sentence_1 = chosen_sentence[0]
+            chosen_sentence_2 = chosen_sentence[1]
+            chosen_sentence_3 = chosen_sentence[2]
+            context['chosen_sentence'] = [(chosen_sentence_1.pk, chosen_sentence_1.content), 
+                                        (chosen_sentence_2.pk, chosen_sentence_2.content), 
+                                        (chosen_sentence_3.pk, chosen_sentence_3.content)]
+            context['chosen_description'] = chosen_question.description
+            context['chosen_standard'] = chosen_question.standard
+            context['form'] = ListWise()
+        else:
+            return render(request, 'sorry.html')
+        response = render(request, 'task.html', context)
+    elif request.method == "POST":
+        if current == 1:
+            context['form'] = PointWise(request.POST)
+        elif current == 2:
+            context['form'] = PairWise(request.POST)
+        elif current == 3:
+            context['form'] = ListWise(request.POST)
+        else:
+            return render(request, 'sorry.html')
+        response = render(request, 'task.html', context)
         response.set_cookie("visited_task", True)
+    else:
+        response = render(request, 'sorry.html')
     return response
 
 def sorry(request):
